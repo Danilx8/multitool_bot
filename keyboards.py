62a65df17b1e71d60ecee5f80980f1f137efc2cd
Danilx8 <lugovskihdanil@yandex.ru>
2023-04-13 21:05:32 +0700

    added an oracle functioning through the external api

--- keyboards.py
+++ keyboards.py
@@ -4,10 +4,20 @@ from aiogram.types import ReplyKeyboardRemove, \
 
 button_to_voice_input = InlineKeyboardButton('Перейти на голосовые сообщения', callback_data='voice')
 button_to_interview = InlineKeyboardButton('Приступить к интервью', callback_data='interview')
+button_to_api = InlineKeyboardButton('Задать вопрос оракулу', callback_data='api')
+button_to_schedule = InlineKeyboardButton('Записать сегодняшнюю посещаемость', callback_data='schedule')
 
-main_menu_kb = InlineKeyboardMarkup(row_width=1).add(button_to_voice_input).add(button_to_interview)
+main_menu_kb = InlineKeyboardMarkup(row_width=1).add(button_to_voice_input).add(button_to_interview).add(button_to_api)
 
 
 leave_voice_input = KeyboardButton("Прервать прослушку!")
 
 leave_voice_input_kb = ReplyKeyboardMarkup(resize_keyboard=True).add(leave_voice_input)
+
+
+leave_question_input = KeyboardButton('Прекратить задавать вопросы')
+
+leave_question_kb = ReplyKeyboardMarkup(resize_keyboard=True).add(leave_question_input)
+
+
+delete_keyboard = ReplyKeyboardRemove()
